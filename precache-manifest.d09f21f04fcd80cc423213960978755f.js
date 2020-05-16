b52b8480c04138175b0f28d8b2d32309e74eb641
ganesh2598 <kratosganesh2598.com>
2020-05-16 11:41:56 +0530

    Updates

---
+++ precache-manifest.d09f21f04fcd80cc423213960978755f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885f60ab09f4cf57bc6befcd543dbd5e",
+    "url": "/TODO-LIST-APP/index.html"
+  },
+  {
+    "revision": "2b7dd48322a051feb00a",
+    "url": "/TODO-LIST-APP/static/css/main.41f04b5b.chunk.css"
+  },
+  {
+    "revision": "69813e6cd732b0a3e467",
+    "url": "/TODO-LIST-APP/static/js/2.13c12ee1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TODO-LIST-APP/static/js/2.13c12ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7dd48322a051feb00a",
+    "url": "/TODO-LIST-APP/static/js/main.9a368f0b.chunk.js"
+  },
+  {
+    "revision": "1b5ad42f557edb0f999d",
+    "url": "/TODO-LIST-APP/static/js/runtime-main.8b7e3a78.js"
+  }
+]);
